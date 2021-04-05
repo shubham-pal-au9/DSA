bcb47134c02315866c0fdd27e09d01a9da7ca0dc
shubham-pal-au9 <spal5776@gmail.com>
2021-04-05 16:16:21 +0530

    union and intersection

--- basic_code/array/union_of_two_array.py
+++ basic_code/array/union_of_two_array.py
@@ -15,10 +15,25 @@ def union(lst1,lst2):
     
     print(count)
 
+def intersection(lst1,lst2):
+    res = []
+    count = 0
+    for i in range(0,len(lst2)):
+        for j in range(0,len(lst1)):
+            if lst2[i] == lst1[j]:
+                res.append(lst2[i])
+
+    mylist = sorted(set(res))
+    
+    for i in range(0,len(mylist)):
+        count = count + 1 
+    print(count)
+
 if __name__ == '__main__':
     
     lst1 = [1,2,3,4,5]
     lst2 = [1,2,3]
     
     union(lst1,lst2)
+    intersection(lst1,lst2)
    
